c8a209931dc4592588f8d89fa0167200407ed726
BcnChsBrgr <yuhinng41@gmail.com>
2024-11-27 00:25:19 +0000

    add security.ts

--- app/src/bootstrap/app.ts
+++ app/src/bootstrap/app.ts
@@ -1,26 +1,14 @@
 import express, { Express } from "express";
-import helmet from "helmet";
 import apis from "../route/apis";
 import pages from "../route/pages";
-import compression from "compression";
-import session from "express-session";
+import security from "../config/security";
 
 const app: Express = express();
+app.use(security);
 
-app.disable("x-powered-by");
-
-app.use(compression());
-app.set("trust proxy", 1);
-app.use(
-    session({
-        secret: "s3Cur3",
-        name: "sessionId",
-    })
-);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(helmet());
 app.use("/", pages);
 app.use("/api", apis);
 
